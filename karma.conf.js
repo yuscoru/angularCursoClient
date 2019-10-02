b048c8243a87a4878794681cae28a37051b9f55e
José Luis Corujo <yuscoru@hotmail.com>
2019-10-02 16:47:51 +0200

    karma sin navegador para evitar que falle en travis

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
+    customLaunchers: {
+      HeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox'
+        ]
+      },
     restartOnFileChange: true
   });
 };
